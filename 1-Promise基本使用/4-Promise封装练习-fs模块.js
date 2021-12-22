d1ff24e0b637d089b3f375d062eab487edefbeca
1603301847 <1903105953@qq.com>
2021-12-22 15:36:45 +0800

    Promise基本使用

---
+++ 1-Promise基本使用/4-Promise封装练习-fs模块.js
@@ -0,0 +1,23 @@
+/**
+ * 封装一个函数 mainReadFile 读取文件内容
+ * 参数: path  文件路径
+ * 返回: promise 对象
+ */
+function mainReadFile(path) {
+    return new Promise((resolve, reject) => {
+        // 读取文件
+        require('fs').readFile(path, (err, data) => {
+            // 判断
+            if (err) reject(err);
+            // 成功
+            resolve(data);
+        });
+    });
+}
+
+mainReadFile('./resource/content.txt').then(value => {
+    // 输出文件内容
+    console.log(value.toString());
+}, reason => {
+    console.log(reason);
+});
