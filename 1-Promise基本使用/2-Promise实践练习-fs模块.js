e3746bd3194d2f48d460554f21da5e1aac424833
1603301847 <1903105953@qq.com>
2021-12-22 09:36:36 +0800

    Promise基本使用

---
+++ 1-Promise基本使用/2-Promise实践练习-fs模块.js
@@ -0,0 +1,28 @@
+// 引入 fs 模块
+const fs = require('fs');
+
+// 回调函数 形式
+// 读取文件
+// fs.readFile('./resource/content.txt', (err, data) => {
+//     // 如果出错 则抛出错误
+//     if(err) throw err;
+//     // 输出文件内容
+//     console.log(data.toString());
+// });
+
+// Promise 形式
+let p = new Promise((resolve, reject) => {
+    fs.readFile('./resource/content.txt', (err, data) => {
+        // 如果出错 则抛出错误
+        if (err) reject(err);
+        // 输出文件内容
+        resolve(data);
+    });
+});
+
+// 调用then
+p.then(value => {
+    console.log(value.toString());
+}, reason => {
+    console.log(reason);
+});
